6a41db0f002b04aeb59ca92b88f8bd9cb62ea84a
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-08-17 23:11:50 -0400

    trying to resolve 'emphasized items issue' and created API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("Linked!");
+var APIkey = "7f6f2d99953fc081fdbbf025eed2d11f";
+var cityName = ;
+console.log(APIkey);
+
+requestUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  cityName +
+  "&appid=" +
+  APIkey;
