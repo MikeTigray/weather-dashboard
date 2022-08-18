b8f04c42e73ad853353f3e3d171a654759668d7e
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-08-18 01:36:48 -0400

    generated API keys from two different weather servers -one for 5 day forecast - and second one to get weather details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,41 @@
 var APIkey = "7f6f2d99953fc081fdbbf025eed2d11f";
-var cityName = ;
-console.log(APIkey);
+var cityName = "Denver";
+var APIkeyForecast = "0e0e192396f34de1bf07cb631356eb62";
+// requestUrl =
+//   "https://api.openweathermap.org/data/2.5/weather?q=" +
+//   cityName +
+//   "&appid=" +
+//   APIkey;
+// console.log(requestUrl);
 
-requestUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
+var requestUrl5 =
+  "https://api.weatherbit.io/v2.0/forecast/daily?city=" +
   cityName +
-  "&appid=" +
-  APIkey;
+  "&key=" +
+  APIkeyForecast;
+
+console.log(requestUrl5);
+
+fetch(requestUrl5)
+  .then(function (res) {
+    console.log(res);
+    return res.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log();
+  });
+
+// fetch(requestUrl)
+//   .then(function (res) {
+//     console.log(res);
+//     return res.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//     console.log();
+//   });
+
+//data.wind.speed; // for Wind speed in MPH
+// data.main.temp// temp in kelvin, needed to change to fahrenheit
+//data.main.humidity// humidity in %
